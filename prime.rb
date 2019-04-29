c91bb696e452e96f0ba309fdd99cb72ebae81dbb
Rina Kawamura <rina.kawamura@yale.edu>
2019-04-29 01:16:28 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
 # Add  code here!
+def prime?(num)
+  array = []
+  if num <= 2
+    if num == 2
+      return true
+    else
+      return false
+    end
+  end
+  for i in 2..(num - 1)
+    array.append(i)
+  end
+  array.each do |element|
+    if (num % element == 0)
+      return false
+    end
+  end
+  return true
+end
